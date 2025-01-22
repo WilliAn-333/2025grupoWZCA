9eb0bb3a0477e7f0498d0a4daffab4d682d177e1
WilliAn-333 <www.pegazo23@gmail.com>
2025-01-21 20:34:01 -0500

    creacion del repositorio

---
+++ src/main/java/ec/edu/espoch/grupo2025wzca/Grupo2025WZCA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.grupo2025wzca;
+
+/**
+ *
+ * @author USER
+ */
+public class Grupo2025WZCA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
